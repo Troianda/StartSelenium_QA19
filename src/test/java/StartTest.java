a4ed0fd0e2385c239b32d072b8b61a636eb91516
Troianda <troiandisa@gmail.com>
2023-06-08 19:53:03 +0300

    8.06

--- src/test/java/StartTest.java
+++ src/test/java/StartTest.java
@@ -24,19 +24,33 @@ public class StartTest {
     public void test() {
         wd.findElement(By.tagName("a"));//tagName[AttributeName='AttributeValue']//tagName.nameClassElement
         wd.findElement(By.cssSelector("a"));//tagName
+        wd.findElement(By.xpath("//a"));//xPath
+
         wd.findElement(By.id("root"));
         wd.findElement(By.cssSelector("#root"));//id
+        wd.findElement(By.xpath("//*[@id='root']"));
+
         wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.xpath("//*[text()='HOME']"));
+        wd.findElement(By.xpath("//*[.='HOME']"));
 
         wd.findElement(By.partialLinkText("HO"));
+
         WebElement element = wd.findElement(By.id("root"));
         List<WebElement> List = wd.findElements(By.tagName("div"));
         System.out.println(List.size());
 
         wd.findElement(By.cssSelector("[href = '/home']"));
+        wd.findElement(By.xpath("//*[href = '/home']"));
+
         wd.findElement(By.cssSelector("[href^= '/ho']"));//найти  по вервым буквам
-        wd.findElement(By.cssSelector("[href$= '/me']"));//найти по последним буквам
+        wd.findElement(By.xpath("//*[starts-with(@href,'/ho']"));
+
+        wd.findElement(By.cssSelector("[href$='/me']")); //найти по последним буквам
+        // no xPath
+
         wd.findElement(By.cssSelector("[href*= '/om']"));// найти по середним  буквам
+        wd.findElement(By.xpath("//*[contains(@href,'/om')]"));
 
     }
         @AfterMethod
